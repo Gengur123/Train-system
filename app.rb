41ed0a07d52f571e53068f0c217aa9215f4a8c98
Gengur123 <meleearthur@gmail.com>
2021-07-28 15:25:06 -0700

    Added a list when editing the trains to locations
    Co-authored-by: Kristen Hopper <hopperdavis@gmail.com>

--- app.rb
+++ app.rb
@@ -9,11 +9,12 @@ require "pg"
 DB = PG.connect({ dbname: 'train_station', host: 'db', user: 'postgres', password: 'password' })
 
 get('/') do
-  redirect to('/trains')
+  redirect to('/station')
 end
 
-get('/trains') do
+get('/station') do
   @trains = Train.all
+  @cities = City.all
   erb(:station)
 end
 
@@ -26,6 +27,32 @@ post('/trains') do
   train = Train.new({:name => name, :id => nil})
   train.save()
   @trains = Train.all
-  redirect to('/trains')
+  redirect to('/station')
 end
 
+get('/trains/:id') do
+  @train = Train.find(params[:id].to_i())
+  erb(:train)
+end
+
+get('/cities/new') do
+  erb(:new_cities)
+end
+
+post('/cities') do
+  name = params[:city_name]
+  city = City.new({:city_name => name, :id => nil})
+  city.save()
+  @cities = City.all
+  redirect to('/station')
+end
+
+get('/trains/:id/edit') do
+  @train = Train.find(params[:id].to_i())
+  @cities = City.all
+  erb(:edit_train)
+end
+
+post('/trains/:id/edit') do
+  @city_name = params[:city_name]
+  
